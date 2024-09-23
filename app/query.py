92dbfd8daaafc94c05d3d735ae01b98bcc948e91
Sacal <andreisacal@yandex.com>
2024-09-23 15:15:26 +0200

    this is a test

---
+++ app/query.py
@@ -0,0 +1,15 @@
+from neo4j import GraphDatabase
+
+if __name__=="__main__":
+
+    URI = "bolt://neo4j-andrei:7687"
+
+    driver = GraphDatabase.driver(URI, auth=("neo4j", "password"))
+
+    while True:
+        userinput = input("Enter your query: ")
+        print(userinput)
+        records, _, _ = driver.execute_query(userinput, database="neo4j")
+
+        for record in records:
+            print(records)
